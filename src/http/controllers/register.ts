719826da0e89399b963751d65e6a5ab0e72d06a9
Heitor Belém <heitorlbelem98@gmail.com>
2024-11-24 01:56:22 -0300

    sends error message to frontend

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -17,7 +17,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     await registerUseCase.execute({ name, email, password })
   } catch(error) {
     if (error instanceof UserAlreadyExistsError) {
-      return reply.status(409).send()
+      return reply.status(409).send({ message: error.message })
     }
     return reply.status(500).send()
   }
