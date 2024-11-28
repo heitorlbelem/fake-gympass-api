7c3d9a061a8e696c2b5b3178ed0b1cdb0ab80cf8
Heitor Belém <heitorlbelem98@gmail.com>
2024-11-28 12:05:41 -0300

    creates isolated test databases for e2e specs

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,34 @@
+import { randomUUID } from 'node:crypto'
 import type { Environment } from 'vitest'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
+
+const prisma = new PrismaClient()
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('setup')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+    process.env.DATABASE_URL = databaseURL
+    execSync('npx prisma migrate deploy')
     return {
-      teardown() {
-        console.log('tear down')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
       },
     }
   },
