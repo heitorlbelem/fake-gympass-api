1a73ffd119a50a38781387e11251f8c097ae905f
Heitor Belém <heitorlbelem98@gmail.com>
2024-11-28 14:32:31 -0300

    create spec for register controller

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,7 +1,21 @@
-import { describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-describe('register', () => {
-  it('should return true', () => {
-    expect(2 + 2).toEqual(4)
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+    expect(response.statusCode).toEqual(201)
   })
 })
